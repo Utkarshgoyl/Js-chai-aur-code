1c88aa4ad90b1086f50e68e111dfb918feaca120
Utkarshgoyl <utkarshgoyal554@gmail.com>
2025-08-18 14:27:56 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const AccountID = "144543"
+let AccountEmail = "Utkarshgoyal554@gmail.com"
+var AccountPassword = "12345678"
+AccountCity = "Jaipur"
+let AccountState = "Rajasthan"
+
+// AccountID = "123456" not allowed 
+
+AccountEmail = "kapil@gmail.com"
+AccountPassword = "1234746"
+AccountCity = "Rewari"
+AccountState = "Haryana"
+
+console.log(AccountID); // we use this line to print one datatype 
+
+/*
+{*//* --> also known as scope}
+prefer not to use var 
+as issue in block scope and functional scope 
+*/
+
+console.table([AccountID, AccountEmail, AccountPassword, AccountState, AccountCity]); // we use this code to print multiple datatype in tabular form
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,38 @@
+"use strict" // treat all JS Code as newer version
+
+//alert( 3 + 3 ) we use it in browser but at now we are using nodejs
+
+console.log( 3 
+    + 
+    3 ); // code readability should be high
+            //     |
+            //     |
+            //    \ /
+
+        console.log("Utkarsh");
+
+
+let Name = "Utarsh"     // string datatype
+let age = 19            // number datatype
+let isLoggedIn = false  // boolean datatype
+let state;              // Undefined
+
+/*
+                 DATATYPES  
+            PRIMITIVE DATATYPES
+    (meaning their values cannot be changed.)
+    number => 2 to power 53
+    bigint => used to store big values {like in stockmarket, facebook, reddit, etc.}
+    string => "" anything in double quote is string
+    boolean => true/false
+    null => standalone value datatype
+    undefined => the variable is defined but the value is not assigned till now
+    symbol => unique id or value
+*/
+
+//         NON-PRIMITIVE (REFERENCE) DATATYPES 
+// (meaning their properties and elements can be changed.)
+// object 
+
+    console.log(typeof undefined); // undefined
+    console.log(typeof null);      // object
