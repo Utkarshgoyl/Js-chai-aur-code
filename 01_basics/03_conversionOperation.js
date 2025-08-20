3947934439c15dde39b5f076ff05114fa3be122e
Utkarshgoyl <utkarshgoyal554@gmail.com>
2025-08-20 17:35:33 +0000

    container config done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,1 +1,19 @@
-let score = "hitesh"
+let score = "utkarsh"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+/*
+"33" => 33  number in a string is converted into number
+"33abc" => NaN (Not an Number) after conversion it gives NaN but still it converted to number
+true => 1; false => 0
+*/
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
